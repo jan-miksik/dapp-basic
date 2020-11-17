354fd25f4ca40f2685e9a501794faf5b0ae7123b
jan-miksik <j@jm>
2020-11-17 13:54:22 +0100

    init commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hellou world dapp on eth smart contract and IPFS</h1>
+      <header className="App-header"></header>
+    </div>
+  );
+}
+
+export default App;
